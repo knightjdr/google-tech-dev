153d9250b01fae69974dbac0a3a114f1be0123a8
James Knight <knightjdr@gmail.com>
2019-01-23 12:22:49 -0500

    Simplify sumNumbers solution

--- sum-numbers/sum-numbers.js
+++ sum-numbers/sum-numbers.js
@@ -1,23 +1,9 @@
 // https://codingbat.com/prob/p121193
 
 const sumNumbers = (str) => {
-  let arr = str.split('');
-  let currentNumber = '';
-  let sum = arr.reduce((runningSum, char) => {
-    if (!isNaN(Number(char)) && char !== ' ') {
-      currentNumber += char;
-      return runningSum;
-    } if (currentNumber) {
-      const newSum = runningSum + Number(currentNumber);
-      currentNumber = '';
-      return newSum;
-    }
-    return runningSum;
-  }, 0);
-  if (currentNumber) {
-    sum += Number(currentNumber);
-  }
-  return sum;
+  const re = new RegExp(/[^0-9]+/g);
+  const numbers = str.replace(re, ',').split(',');
+  return numbers.reduce((sum, number) => sum + Number(number), 0);
 };
 
 module.exports = sumNumbers;
